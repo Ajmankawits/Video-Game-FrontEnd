270c21c34dda48d42a1c48912323d13f3a02e206
Alex <thatsoalex@gmail.com>
2022-05-17 14:40:48 -0500

    Added Dependencies + Added Functionality to DisplayPlatformStats.jsx

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
-import 
+import GlobalSalesChart from './components/charts/DisplayPlatformStats';
 
 function App() {
 
-  const [videoGames, setVideoGames] = useState([]);
-
-  useEffect(() => {
-    getVideoGames();
-  }, []);
-
+  const [games, setVideoGames] = useState([]);
+  
   async function getVideoGames(){
     try{
       debugger;
-      const response = await axios.get('');
+      const response = await axios.get('https://localhost:7260/api/games');
       setVideoGames(response.data);
     } catch(ex){
       console.log('ERROR in getVideoGames EXCEPTION: ${ex}')
     }
-  }
+  };
+  //useEffect will run when the page loads
+  useEffect(() => {
+    getVideoGames();
+  }, []);
+
 
   return (
     <div>
-     <DisplayPlatformStats videoGames={videoGames}/>
+     <GlobalSalesChart videoGames={games}/>
     </div>
   );
 }
--- src/components/charts/DisplayPlatformStats.jsx
+++ src/components/charts/DisplayPlatformStats.jsx
@@ -1,29 +1,67 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Chart } from 'react-google-charts';
 
-const DisplayPlatformStats = ({videoGames}) => {
-
-    function generateDataFormChart(){
-
-        console.log(videoGames)
-
-        const data = [
-            ["Platform", "Sales", { role: "style" }],
-            ["PS3", 8.94, "#b87333"],
-            ["Silver", 10.49, "silver"],
-            ["Gold", 19.3, "gold"],
-            ["Platinum", 21.45, "color: #e5e4e2"],
-        ];
-
-        return data;
+const GlobalSalesChart = (props) => {
+    const option = {
+      title: "Global Sales Per Platform",
+      vAxis: {title: 'Sales per Million'},
+      hAxis: {title: 'Platform'},
+      legend:"none"
     }
+    function Data() {
 
+        
+   
+        let filteredGames = props.videosGames.filter((games) => games.year >= 2013);
+ 
+        let gameplatform = filteredGames.map((game) => {
+            return game.platform;
+        });
+  
+        let newplatform = [...new Set(gameplatform)];
+  
+    
+  
+        let platformArrays = newplatform.map((platform) => {
+        let gamingplatforms = filteredGames.filter(
+          (game) => game.platform === platform
+        );
+        let totalsales = gamingplatforms.map((game) => {
+          return game.globalSales;
+        });
+  
+        let sum = totalsales
+          .reduce((total, currentnum) => total + currentnum, 0)
+          .toFixed(2);
+  
+  
+      function choose(){
+        let color = (['#a2a2d0', 'light blue', '#1e90ff','navy blue','#3f00ff'])
+        let index = Math.floor(Math.random()* color.length);
+        return color[index]
+      }
+  
+  
+    
+        
+              
+            
+        
+  
+        return [platform, parseFloat(sum),choose()];
+      });
+  
+      const data = [
+        ["Platform", "Sales(per million)", { role: "style" }],
+        ...platformArrays,
+      ]; // CSS-style declaration
+      return data;
+    }
+  
     return (
-        <div>
-            <h1>Platform By Global Sales in Millions</h1>
-            <Chart chartType="ColumnChart" width="100%" height="400px" data={generateDataFormChart()} />
-        </div>
+      <div className="box">
+      <Chart chartType="ColumnChart" width="100%" height="400px" data={Data()} options={option}/></div>
     );
-}
-
-export default DisplayPlatformStats;
+  };
+  
+  export default GlobalSalesChart;
