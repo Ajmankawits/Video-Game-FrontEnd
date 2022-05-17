9f6f5b87690180d787ea3859d3905307d23e9c2c
Alex <thatsoalex@gmail.com>
2022-05-17 16:08:45 -0500

    Created + Added Functionality to SampleQuestion.jsx

---
+++ src/components/charts/SampleQuestion.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Chart } from "react-google-charts";
+
+export const options = {
+  title: "Top 5 Ranked Games ",
+  curveType: "function",
+  legend: { position: "bottom" },
+  vAxis: {title: 'Rank', baseline:15},
+  hAxis: {title: 'Year', baseline:2010},
+  colorAxis: {colors: ['red', 'black']},
+  
+};
+const SampleQuestion = (props) => {
+  function topfivegames() {
+    let Bestgames = props.videosGames.filter(
+      (element) => element.rank >= 1 && element.rank < 5
+    );
+    let newGames =Bestgames.map((game) => {
+      return [  game.name,game.year,game.rank];
+    });
+    
+    const data = [[ "Name", "Year","Rank"], ...newGames,];
+    console.log(data)
+    return data;
+  }
+
+  return (
+    <Chart
+      className="bubble-chart"
+      chartType="BubbleChart"
+      width="100%"
+      height="400px"
+      data={topfivegames()}
+      options={options}
+    />
+  );
+};
+
+
+
+export default SampleQuestion;
+
